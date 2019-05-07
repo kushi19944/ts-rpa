fa4c0de8a4f28e652550a3bdffbcd03fa15e1a1f
hayamin <dev@hayam.in>
2019-05-07 15:10:18 +0900

    Add eslint

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,14 +1,14 @@
 import { configure, getLogger } from "log4js";
 
 configure({
-    appenders: {
-        default: { type: "stdout" },
-        system: { type: "stderr" }
-    },
-    categories: {
-        default: { appenders: ["default"], level: "all" },
-        system: { appenders: ["system"], level: "error" }
-    }
+  appenders: {
+    default: { type: "stdout" },
+    system: { type: "stderr" }
+  },
+  categories: {
+    default: { appenders: ["default"], level: "all" },
+    system: { appenders: ["system"], level: "error" }
+  }
 });
 
 export default getLogger("default");
--- src/bin.ts
+++ src/bin.ts
@@ -1,36 +1,37 @@
 #!/usr/bin/env node
 import { readFileSync } from "fs";
-import Module = require("module");
 import * as ts from "typescript";
 import { Script } from "vm";
 
+import Module = require("module");
+
 (async () => {
-    const filename = process.argv[2];
+  const filename = process.argv[2];
 
-    const code = readFileSync(filename, "utf8");
-    const options: ts.CompilerOptions = {
-        module: ts.ModuleKind.CommonJS,
-        target: ts.ScriptTarget.ES2018
-    };
-    const transpiledCode = ts.transpile(code, options);
-    const script = new Script(transpiledCode, {
-        displayErrors: true,
-        filename
-    });
+  const code = readFileSync(filename, "utf8");
+  const options: ts.CompilerOptions = {
+    module: ts.ModuleKind.CommonJS,
+    target: ts.ScriptTarget.ES2018
+  };
+  const transpiledCode = ts.transpile(code, options);
+  const script = new Script(transpiledCode, {
+    displayErrors: true,
+    filename
+  });
 
-    const module = new Module(filename);
-    module.filename = filename;
-    module.paths = (Module as any)._nodeModulePaths(filename);
+  const module = new Module(filename);
+  module.filename = filename;
+  module.paths = (Module as any)._nodeModulePaths(filename); // eslint-disable-line no-underscore-dangle
 
-    (global as any).exports = module.exports;
-    (global as any).module = module;
-    (global as any).require = module.require.bind(module);
+  (global as any).exports = module.exports;
+  (global as any).module = module;
+  (global as any).require = module.require.bind(module);
 
-    const hardFixScript = new Script("exports = module.exports", {
-        displayErrors: true,
-        filename
-    });
+  const hardFixScript = new Script("exports = module.exports", {
+    displayErrors: true,
+    filename
+  });
 
-    await hardFixScript.runInThisContext();
-    await script.runInThisContext();
+  await hardFixScript.runInThisContext();
+  await script.runInThisContext();
 })();
