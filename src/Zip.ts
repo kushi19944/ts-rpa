75471b73646e1d7918ad8effd2df8ad464adc9c2
rapwind <hayami_rikuo@cyberagent.co.jp>
2019-05-23 01:45:50 +0900

    Add unzip(decompress) method

---
+++ src/Zip.ts
@@ -0,0 +1,35 @@
+import * as path from "path";
+import * as fs from "fs";
+import * as unzipper from "unzipper";
+import Logger from "./Logger";
+
+export class Zip {
+  private static outDir: string = process.env.WORKSPACE_DIR || "./";
+
+  private constructor() {} // eslint-disable-line no-useless-constructor, no-empty-function
+
+  private static async writeFileSync(file): Promise<string> {
+    fs.writeFileSync(file.path, await file.buffer());
+    return file.path;
+  }
+
+  public static async decompress(params: {
+    filename: string;
+  }): Promise<string[]> {
+    const filePath = path.join(this.outDir, params.filename);
+    Logger.debug("Zip.extract", params);
+    const directory = await unzipper.Open.file(filePath);
+    const results = [];
+    for (let i = 0; i < directory.files.length; i += 1) {
+      const file = directory.files[i];
+      if (file.path.slice(-1) === "/") {
+        fs.mkdirSync(path.join(this.outDir, file.path));
+      } else {
+        results.push(this.writeFileSync(file));
+      }
+    }
+    return Promise.all(results);
+  }
+}
+
+export default Zip;
