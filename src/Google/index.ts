bf6d368b12dfea500f6ef5e512892fd7abf5ade7
Yuta Kosuge <ydt.hran2@gmail.com>
2019-06-17 16:46:47 +0900

    Fix code completion

--- src/Google/index.ts
+++ src/Google/index.ts
@@ -1,14 +1,14 @@
 import GoogleAuth from "./Auth";
-import GoogleSpreadsheet from "./Spreadsheet";
-import GoogleDrive from "./Drive";
-import GoogleGmail from "./Gmail";
+import * as GoogleSpreadsheet from "./Spreadsheet";
+import * as GoogleDrive from "./Drive";
+import * as GoogleGmail from "./Gmail";
 
 export namespace RPA {
   export namespace Google {
     export const { authorize } = GoogleAuth.instance;
-    export const Spreadsheet = GoogleSpreadsheet.instance;
-    export const Drive = GoogleDrive.instance;
-    export const Gmail = GoogleGmail.instance;
+    export const Spreadsheet = GoogleSpreadsheet.default.instance;
+    export const Drive = GoogleDrive.default.instance;
+    export const Gmail = GoogleGmail.default.instance;
   }
 }
 
