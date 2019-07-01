733401959d2c4f1dce8d800ca4648e2bf90faee4
rapwind <hayami_rikuo@cyberagent.co.jp>
2019-07-01 16:01:23 +0900

    Add Request.download method

--- src/Request.ts
+++ src/Request.ts
@@ -1,16 +1,47 @@
 import nodeFetch, { RequestInit, Response } from "node-fetch";
 import * as FormData from "form-data";
+import * as path from "path";
+import * as fs from "fs";
 import Logger from "./Logger";
 
 export namespace RPA {
   export class Request {
     private constructor() {} // eslint-disable-line no-useless-constructor, no-empty-function
 
+    private static outDir: string = process.env.WORKSPACE_DIR || "./";
+
     public static FormData = FormData;
 
     public static fetch(url: string, init?: RequestInit): Promise<Response> {
       Logger.debug("Request.fetch", url, init);
       return nodeFetch(url, init);
+    }
+
+    public static async download(
+      filename: string,
+      url: string,
+      init?: RequestInit
+    ): Promise<Response> {
+      Logger.debug("Request.download", url, init);
+      const res = await nodeFetch(url, init);
+      const fileStream = fs.createWriteStream(path.join(this.outDir, filename));
+      return new Promise(
+        (resolve, reject): void => {
+          res.body.pipe(fileStream);
+          res.body.on(
+            "error",
+            (err): void => {
+              reject(err);
+            }
+          );
+          fileStream.on(
+            "finish",
+            (): void => {
+              resolve(res);
+            }
+          );
+        }
+      );
     }
   }
 }
