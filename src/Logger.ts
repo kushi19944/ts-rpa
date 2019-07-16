bed593bf2b33e9e3fa5a8c3525740a89c8f9db62
rapwind <hayami_rikuo@cyberagent.co.jp>
2019-07-16 17:34:37 +0900

    Add stackdriver logging format

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,9 +1,28 @@
-import { configure, getLogger } from "log4js";
+import { configure, getLogger, addLayout, LoggingEvent } from "log4js";
 
+const layout = (): ((logEvent: LoggingEvent) => string) => (
+  logEvent: LoggingEvent
+): string => {
+  return JSON.stringify({
+    time: logEvent.startTime,
+    severity:
+      logEvent.level.levelStr === "WARN" ? "WARNING" : logEvent.level.levelStr,
+    message: logEvent.data.reduce(
+      (a, c): string => `${a} ${JSON.stringify(c)}`
+    ),
+    data: logEvent.data
+  });
+};
+
+let layoutType = "coloured";
+if (process.env.KUBERNETES_SERVICE_HOST) {
+  addLayout("stackdriver", layout);
+  layoutType = "stackdriver";
+}
 configure({
   appenders: {
-    default: { type: "stdout" },
-    system: { type: "stderr" }
+    default: { type: "stdout", layout: { type: layoutType } },
+    system: { type: "stderr", layout: { type: layoutType } }
   },
   categories: {
     default: { appenders: ["default"], level: "all" },
@@ -16,6 +35,11 @@ export namespace RPA {
   export const SystemLogger = getLogger("system");
 }
 export default RPA.Logger;
+
+/* eslint-disable no-console */
+console.error = (message?: any, ...optionalParams: any[]): void => {
+  RPA.SystemLogger.error(message, optionalParams);
+};
 
 /**
  * @ignore
