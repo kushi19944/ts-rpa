0e40397d80200e1fa7351d5daae980cd06834de3
Yuta Kosuge <ydt.hran2@gmail.com>
2019-07-17 12:11:27 +0900

    Add File.list(), File.listFiles()

--- src/File.ts
+++ src/File.ts
@@ -85,6 +85,27 @@ export namespace RPA {
       const file = fs.readFileSync(path.join(this.outDir, params.filename));
       return fileType(file).mime;
     }
+
+    public static list(params?: { dirname: string }): string[] {
+      const dirname = (params && params.dirname) || "./";
+      Logger.debug("File.list", { dirname });
+      return fs.readdirSync(path.join(this.outDir, dirname));
+    }
+
+    public static listFiles(params?: { dirname: string }): string[] {
+      const dirname = (params && params.dirname) || "./";
+      Logger.debug("File.listFiles", { dirname });
+      return File.list(params).filter(
+        (filename): boolean =>
+          File.isFile({ filename: path.join(dirname, filename) })
+      );
+    }
+
+    public static isFile(params: { filename: string }): boolean {
+      Logger.debug("File.isFile", params);
+      const file = path.resolve(path.join(this.outDir, params.filename));
+      return File.exists(params) && fs.lstatSync(file).isFile();
+    }
   }
 }
 
